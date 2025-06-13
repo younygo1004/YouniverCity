365d09a2cf427143bded78149f5e969adcc033d0
younygo1004 <younygo1004@gmail.com>
2025-06-13 17:53:33 +0900

    Spring Boot init

---
+++ youniverCity_backend/src/main/java/city/youniverse/younivercity/YouniverCityApplication.java
@@ -0,0 +1,13 @@
+package city.youniverse.younivercity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouniverCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouniverCityApplication.class, args);
+    }
+
+}
---
+++ youniverCity_backend/src/test/java/city/youniverse/younivercity/YouniverCityApplicationTests.java
@@ -0,0 +1,13 @@
+package city.youniverse.younivercity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouniverCityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
